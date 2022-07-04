1b274ccd1486ecb37935b7e74e1b9948c196889e
woongs <woongs.rich@kakaocorp.com>
2022-07-04 19:39:19 +0900

    DrawingFactory 추가

--- swift-drawingapp/Presentation/DrawingFactory.swift
+++ swift-drawingapp/Presentation/DrawingFactory.swift
@@ -6,4 +6,43 @@
 //
 
 
-import Foundation
+import UIKit
+
+struct DrawingFactory {
+
+    func makeRectView(with rect: Rectangle) -> UIView {
+        let view = UIView(frame: rect.frame)
+        view.backgroundColor = convertSystemColor(with: rect.color)
+        return view
+    }
+}
+
+extension DrawingFactory {
+    
+    private func convertSystemColor(with color: DrawColor) -> UIColor {
+        switch color {
+        case .blue:
+            return .systemBlue
+        case .cyan:
+            return .systemCyan
+        case .brown:
+            return .systemBrown
+        case .mint:
+            return .systemMint
+        case .pink:
+            return .systemPink
+        case .orange:
+            return .systemOrange
+        case .yellow:
+            return .systemYellow
+        case .teal:
+            return .systemTeal
+        case .indigo:
+            return .systemIndigo
+        case .purple:
+            return .systemPurple
+        case .green:
+            return .systemGreen
+        }
+    }
+}
