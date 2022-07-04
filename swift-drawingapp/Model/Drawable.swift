bcb38d09388132a26e4ea48e1a386fed8765dc47
woongs <woongs.rich@kakaocorp.com>
2022-07-04 19:36:28 +0900

    파일 생성

---
+++ swift-drawingapp/Model/Drawable.swift
@@ -0,0 +1,9 @@
+//
+//  Drawable.swift
+//  swift-drawingapp
+//
+//  Copyright (c) 2022 woongs All rights reserved.
+//
+
+
+import Foundation
---
+++ swift-drawingapp/Presentation/DrawingFactory.swift
@@ -0,0 +1,9 @@
+//
+//  DrawingFactory.swift
+//  swift-drawingapp
+//
+//  Copyright (c) 2022 woongs All rights reserved.
+//
+
+
+import Foundation
